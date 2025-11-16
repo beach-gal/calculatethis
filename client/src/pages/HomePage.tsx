d0be79726075d3613280b139d1823ce6d3e101d6
Patti Oar <pattioar@gmx.com>
2025-11-16 14:32:09 -0500

    Create HomePage.tsx

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to CalculateThis.org</h1>
+      <p>Select a calculator from the categories.</p>
+    </div>
+  );
+}
